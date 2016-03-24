1153df1b5a220b9a6bb490dee2380691049f4f12
Thomas Pasque <tpasque@gmail.com>
2016-03-23 18:08:18 -0600

    Readme added

--- js/controllers/main_controller.js
+++ js/controllers/main_controller.js
@@ -1,5 +1,5 @@
 app.controller('MainController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
-  $scope.works = "hello it works, and you are on the home page"
+  // $scope.works = "hello it works, and you are on the home page"
 
   $scope.submitSearch = function(term){
       var term = $scope.search
